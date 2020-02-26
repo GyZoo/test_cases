ad77eba5f181365c6f2dd84783cc284d188e2d55
FWO <GyZoo@yandex.ru>
2020-02-27 01:20:48 +0300

    adding coments

--- Python test cases/Hanoi tower.py
+++ Python test cases/Hanoi tower.py
@@ -1,12 +1,12 @@
-N = int(input())
+N = int(input())  # Вводим количество дисков
 
 
-def Hanoi(n, k, m):
+def Hanoi(n, k, m):  # Объявляем фунцию
     if n == 0:
         return
     p = 6 - k - m
     Hanoi(n - 1, k, p)
-    print(k, '->', m)
+    print(k, '->', m)  # Выводим пошаговые действия
     Hanoi(n - 1, p, m)
 
 
